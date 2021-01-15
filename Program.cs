7d2411b168850bd2eb4f52b9c7e077651f4d184e
mhamad9999 <77507252+mhamad9999@users.noreply.github.com>
2021-01-15 21:33:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              
+            int age = 0;
+            Console.WriteLine("enter your age ");
+            age = Convert.ToInt16(Console.ReadLine());
+            int birth = 2021 - age;
+            Console.WriteLine(" birth =  " +birth);
+        }
+    }
+}
